50da6710447f8d18983c4901bdd9222c771d77a6
Evans Mungai <thetracker254@gmail.com>
2023-11-01 13:49:02 +0300

    Update Savings Account to attract bonus in the deposit method

--- src/Account.java
+++ src/Account.java
@@ -2,37 +2,49 @@ class BankAccount {
     int acNo;
     String acName, acPhone, acEmail;
     double AcBal;
-    public BankAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal){
-        this.acNo=acNo;
-        this.acName=acName;
-        this.acPhone=acPhone;
-        this.acEmail=acEmail;
-        this.AcBal=AcBal;
+
+    public BankAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
+        this.acNo = acNo;
+        this.acName = acName;
+        this.acPhone = acPhone;
+        this.acEmail = acEmail;
+        this.AcBal = AcBal;
     }
-    public void accountholder(){
-        System.out.println("Account Number: "+acNo);
-        System.out.println("Account Holder Name: "+acName);
-        System.out.println("Account Holder Address: "+acPhone);
-        System.out.println("Account Holder Phone: "+acEmail);
-        System.out.println("Account Holder Balance: "+AcBal);
+
+    public void accountholder() {
+        System.out.println("Account Number: " + acNo);
+        System.out.println("Account Holder Name: " + acName);
+        System.out.println("Account Holder Address: " + acPhone);
+        System.out.println("Account Holder Phone: " + acEmail);
+        System.out.println("Account Holder Balance: " + AcBal);
     }
-    public void deposit(double amount){
+
+    public void deposit(double amount) {
         AcBal += amount;
         System.out.println("Deposited: $" + amount);
     }
-    public void displayBalance(){
-        System.out.println("Account Number: " + acNo+ "\tBalance: $" + AcBal);
+
+    public void displayBalance() {
+        System.out.println("Account Number: " + acNo + "\tBalance: $" + AcBal);
     }
 }
 
-class SavingsAccount extends BankAccount{
-    public SavingsAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal){
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
         super(acNo, acName, acPhone, acEmail, AcBal);
     }
+
+    public void deposit(double amount) {
+        double bonus = 0.02 * amount;
+        System.out.println("Deposited: $" + amount);
+        amount += bonus;
+        AcBal += amount;
+        System.out.println("Bonus: $" +bonus);
+    }
 }
 
-class CurrentAccount extends BankAccount{
-    public CurrentAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal){
+class CurrentAccount extends BankAccount {
+    public CurrentAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
         super(acNo, acName, acPhone, acEmail, AcBal);
     }
 }
